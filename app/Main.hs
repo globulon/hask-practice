858c9c84acce5413cf807684f14e0e425890914e
Marc-Daniel Ortega <globulon@gmail.com>
2019-04-14 00:43:24 +0100

    play with base algebras

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
-import Lib
+import Algebras
+
+a::[Integer]
+a = [2]
 
 main :: IO ()
-main = someFunc
+main =
+  do
+    print (a |+| zero |+| a |+| zero)
+    print (mapf (* 2) [1,2,3])
